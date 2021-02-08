37c4661f64aea885913185216b4e072853435182
unknown <aymensekhri20@gmail.com>
2021-02-08 18:24:31 +0100

    Add the main files

---
+++ Main.java
@@ -0,0 +1,10 @@
+import student.*;
+import rate.*;
+import material.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("INTTIC Semester Rate"); 
+    }
+}
