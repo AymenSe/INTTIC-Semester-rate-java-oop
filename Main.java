d15c56e735ee2dfb9940d05eec6f960fb6ff8d6e
unknown <aymensekhri20@gmail.com>
2021-02-09 05:39:08 +0100

    Create Student class

--- Main.java
+++ Main.java
@@ -1,10 +1,40 @@
 import student.*;
 import rate.*;
+
+import java.util.ArrayList;
+import java.util.Scanner; 
+
+
 import material.*;
 
 
 public class Main {
     public static void main(String[] args) {
-       System.out.println("INTTIC Semester Rate"); 
+        System.out.println("INTTIC Semester Rate");
+        System.out.println("-------------------");
+        ArrayList<Student> students = addStudent(2, "Aymen", "Sekhri", 43);
+        for (int i = 0; i < students.size(); i++) {
+            students.get(i).getInfos();
+            System.out.println("-------------------");
+        }
+    }
+
+    public static ArrayList<Student> addStudent(int lengthStudents, String firstName, String lastName, int ige) {
+        ArrayList<Student> students = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int j = 1;
+        while (j <= lengthStudents) {
+            System.out.println("Student " + j + " Informations:");
+            System.out.print("Enter FirstName: ");
+            String firstNameInput = sc.nextLine();
+            System.out.print("Enter LastName: ");
+            String lastNameInput = sc.nextLine();
+            System.out.print("Enter Level: ");
+            int igeInput = sc.nextInt();
+            System.out.println("Successfully created!");
+            students.add(new Student(firstNameInput, lastNameInput, igeInput));
+            j++;           
+        }
+        return students;
     }
 }
--- student/Student.java
+++ student/Student.java
@@ -1,5 +1,24 @@
 package student;
 
 public class Student {
-    
+
+    // Private Attributs
+    private String firstName;
+    private String lastName;
+    private int level;
+    private float finalRate = 0;
+
+    // Constructor
+    public Student(String firstName, String lastName, int level) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.level = level;
+    }
+
+    // Methods
+    public void getInfos() {
+        System.out.println("Full Name: " + this.firstName + " " + this.lastName);
+        System.out.println("Level: IGE" + this.level);
+        System.out.println("Final Rate: " + this.finalRate);
+    }
 }
